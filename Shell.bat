9be4a3613867c874bda08a75769af13a8ae05614
Máté Cserép <mcserep@outlook.com>
2018-07-27 19:49:02 +0200

    Follow up tool renaming in Shell.bat

--- Shell.bat
+++ Shell.bat
@@ -18,7 +18,7 @@ if %ERRORLEVEL% neq 0 (
 )
 
 :: Check CloudTools
-where dem_compare >nul 2>&1
+where dem_diff >nul 2>&1
 if %ERRORLEVEL% neq 0 (
   echo CloudTools not found.
   exit /b
@@ -29,9 +29,9 @@ echo.
 
 @if [%1]==[] (
   echo Available programs:
-  echo  dem_compare
-  echo  dem_filter
-  echo  ahn_buildings
+  echo  dem_diff
+  echo  dem_mask
+  echo  ahn_buildings_sim
   echo  ahn_buildings_par
   echo  ahn_buildings_agg
   echo  ahn_buildings_ver
