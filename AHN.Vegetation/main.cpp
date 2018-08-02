c2241f813ff18b7c45158dd005396049bfbaaed8
Máté Cserép <mcserep@outlook.com>
2018-08-02 19:30:00 +0200

    AHN.Vegetation: added console argument validation and support for the --quiet flag.

--- AHN.Vegetation/main.cpp
+++ AHN.Vegetation/main.cpp
@@ -5,7 +5,7 @@
 #include <gdal_priv.h>
 
 #include <CloudTools.Common/IO.h>
-#include <Cloudtools.DEM/Comparers/Difference.hpp>
+#include <CloudTools.DEM/Comparers/Difference.hpp>
 #include <CloudTools.Common/Reporter.h>
 #include "NoiseFilter.h"
 
@@ -21,6 +21,7 @@ int main(int argc, char* argv[])
 	std::string DSMinputPath;
 	std::string outputPath = (fs::current_path() / "out.tif").string();
 
+	// Read console arguments
 	po::options_description desc("Allowed options");
 	desc.add_options()
 		("dtm-input-path,t", po::value<std::string>(&DTMinputPath), "DTM input path")
@@ -35,41 +36,82 @@ int main(int argc, char* argv[])
 	po::store(po::parse_command_line(argc, argv, desc), vm);
 	po::notify(vm);
 
+	// Argument validation
 	if (vm.count("help"))
 	{
-		std::cout << "Compares DEMs of same area to retrieve differences." << std::endl;
+		std::cout << "Compares an AHN-2 and AHN-3 tile pair and filters out changes in vegtation." << std::endl;
 		std::cout << desc << std::endl;
 		return Success;
 	}
 
+	bool argumentError = false;
+	if (!vm.count("dsm-input-path"))
+	{
+		std::cerr << "Surface input file is mandatory." << std::endl;
+		argumentError = true;
+	}
+
+	if (!vm.count("dtm-input-path"))
+	{
+		std::cerr << "Terrain input file is mandatory." << std::endl;
+		argumentError = true;
+	}
+
+	if (!fs::exists(DSMinputPath))
+	{
+		std::cerr << "The surface input file does not exist." << std::endl;
+		argumentError = true;
+	}
+	if (!fs::exists(DTMinputPath))
+	{
+		std::cerr << "The terrain input file does not exist." << std::endl;
+		argumentError = true;
+	}
+
+	if (argumentError)
+	{
+		std::cerr << "Use the --help option for description." << std::endl;
+		return InvalidInput;
+	}
+
+	// Program
 	Reporter *reporter = vm.count("verbose")
 		? static_cast<Reporter*>(new TextReporter())
 		: static_cast<Reporter*>(new BarReporter());
 
-	GDALAllRegister();
-	/*Transformation *comparison = new Difference<float>({ DTMinputPath, DSMinputPath }, "CHM.tif");
+	if (!vm.count("quiet"))
+		std::cout << "=== AHN Vegetation Filter ===" << std::endl;
 
-	comparison->progress = [&reporter](float complete, const std::string &message)
+	GDALAllRegister();
+
+	// Generate CHM
+	Difference<float> *comparison = new Difference<float>({ DTMinputPath, DSMinputPath }, "CHM.tif");
+	if (!vm.count("quiet"))
 	{
-		reporter->report(complete, message);
-		return true;
-	};
+		comparison->progress = [&reporter](float complete, const std::string &message)
+		{
+			reporter->report(complete, message);
+			return true;
+		};
+	}
 
 	comparison->execute();
 	delete comparison;
-
-	reporter->reset();*/
-
+	
+	// Vegetation filter
+	reporter->reset();
 	NoiseFilter* filter = new NoiseFilter("CHM.tif", outputPath, 3);
-
-	filter->progress = [&reporter](float complete, const std::string &message)
+	if (!vm.count("quiet"))
 	{
-		reporter->report(complete, message);
-		return true;
-	};
+		filter->progress = [&reporter](float complete, const std::string &message)
+		{
+			reporter->report(complete, message);
+			return true;
+		};
+	}
 
-	filter->execute();	
+	filter->execute();
 	delete filter;
-
+	delete reporter;
 	return Success;
 }
