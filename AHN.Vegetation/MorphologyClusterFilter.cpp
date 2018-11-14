1acebbbc085a34cbfbcf483b47dc72e2471d9395
efekane <anett.fekete@ericsson.com>
2018-11-14 12:03:07 +0100

    AHN.Vegetation: started fixing the MorphologicalClusterFilter, completed a condition in the Erosion part.

--- AHN.Vegetation/MorphologyClusterFilter.cpp
+++ AHN.Vegetation/MorphologyClusterFilter.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <algorithm>
+#include <iostream>
 
 #include "MorphologyClusterFilter.h"
 
@@ -29,8 +30,15 @@ void MorphologyClusterFilter::onExecute()
 				counter = 0;
 				for (int i = p.getX() - 1; i <= p.getX() + 1; i++)
 					for (int j = p.getY() - 1; j <= p.getY() + 1; j++)
-						if (clusterMap.clusterIndex(i, j) == clusterMap.clusterIndex(p.getX(), p.getY()))
-							++counter;
+                    {
+                      OGRPoint point(i, j);
+                      if (std::find_if(clusterMap.points(index).begin(),
+                                       clusterMap.points(index).end(),
+                                       [&point](const OGRPoint& p) { return p.Equals(&point); })
+                          != clusterMap.points(index).end()
+                          && clusterMap.clusterIndex(i, j) == clusterMap.clusterIndex(p.getX(), p.getY()))
+                        ++counter;
+                    }
 
 				if (counter < this->threshold);
 					pointSet.emplace_back(p);
