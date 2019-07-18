dbf93246d8b4992bc34a563846a176ed6234e3c8
efekane <anett.fekete@ericsson.com>
2019-07-18 11:26:23 +0200

    AHN.Vegetation: added VolumeDifference class.

---
+++ AHN.Vegetation/VolumeDifference.h
@@ -0,0 +1,37 @@
+#ifndef CLOUDTOOLS_VOLUMEDIFFERENCE_H
+#define CLOUDTOOLS_VOLUMEDIFFERENCE_H
+
+#include <CloudTools.DEM/ClusterMap.h>
+
+using namespace CloudTools::DEM;
+
+template <typename T>
+class VolumeDifference
+{
+public:
+  enum AHN {AHN2, AHN3};
+
+  ClusterMap ahn2, ahn3;
+  T distance;
+
+  double ahn2_fullVolume, ahn3_fullVolume;
+  std::map<GUInt32, double> ahn2_lonelyVolume, ahn3_lonelyVolume;
+  std::map<std::pair<GUInt32, GUInt32>, double> diffs;
+
+  VolumeDifference(ClusterMap& ahn2_map,
+                  ClusterMap& ahn3_map,
+                  T& distance)
+                  : ahn2(ahn2_map), ahn3(ahn3_map), distance(distance)
+  {
+    calculateVolume();
+  }
+
+private:
+  void calculateVolume();
+
+  std::pair<double, std::map<GUInt32, double>> calculateLonelyEpochVolume(AHN epoch, ClusterMap& map);
+
+  void calculateDifference();
+};
+
+#endif //CLOUDTOOLS_VOLUMEDIFFERENCE_H
