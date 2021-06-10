cc249cfa64bae756ec2ae69ed51f21d6e34107ee
Fekete Anett <hutche@inf.elte.hu>
2021-06-10 08:05:53 +0000

    Early break for Hausdorff-distance

--- AHN.Vegetation/HausdorffDistance.cpp
+++ AHN.Vegetation/HausdorffDistance.cpp
@@ -7,37 +7,35 @@ namespace Vegetation
 void HausdorffDistance::onExecute()
 {
 	progress(0.f, "Performig Hausdorff-distance based cluster pairing.");
-	
-	std::vector<double> distances;
-	double minDistance;
 
-	for (GUInt32 index : AHN2ClusterMap.clusterIndexes())
+	// AHN2 -> AHN3
+	for (const GUInt32 ahn2Index : AHN2ClusterMap.clusterIndexes())
 	{
-		for (GUInt32 otherIndex : AHN3ClusterMap.clusterIndexes())
+		for (const GUInt32 ahn3Index : AHN3ClusterMap.clusterIndexes())
 		{
-			auto ahn3Center = AHN3ClusterMap.center2D(otherIndex);
-			auto ahn2Center = AHN2ClusterMap.center2D(index);
+			auto ahn3Center = AHN3ClusterMap.center2D(ahn3Index);
+			auto ahn2Center = AHN2ClusterMap.center2D(ahn2Index);
 			double distance = ahn2Center.Distance(&ahn3Center);
 			if (distance >= maximumDistance)
 				continue;
 
-			distances.clear();
-			for (const OGRPoint& point : AHN2ClusterMap.points(index))
+			double cmax = 0;
+			for (const OGRPoint& ahn2Point : AHN2ClusterMap.points(ahn2Index))
 			{
-				std::vector<OGRPoint> ahn3Points = AHN3ClusterMap.points(otherIndex);
-				minDistance = point.Distance(&ahn3Points[0]);
-				for (const OGRPoint& otherPoint : ahn3Points)
+				double cmin = std::numeric_limits<double>::max();
+				for (const OGRPoint& ahn3Point : AHN3ClusterMap.points(ahn3Index))
 				{
-					double dist = point.Distance(&otherPoint);
-					if (dist < minDistance)
+					double dist = ahn2Point.Distance(&ahn3Point);
+					if (dist < cmax)
 					{
-						minDistance = dist;
+						cmin = 0;
+						break;
 					}
+					cmin = std::min(dist, cmin);
 				}
-				distances.push_back(minDistance);
+				cmax = std::max(cmin, cmax);
 			}
-			ahn2HausdorffDistances.emplace(std::make_pair(std::make_pair(index, otherIndex),
-			                                              *std::max_element(distances.begin(), distances.end())));
+			ahn2HausdorffDistances.emplace(std::make_pair(std::make_pair(ahn2Index, ahn3Index), cmax));
 		}
 	}
 
@@ -45,134 +43,135 @@ void HausdorffDistance::onExecute()
 	if (progress)
 		progress(0.35f, "AHN2 to AHN3 distances calculated.");
 
-	for (GUInt32 index : AHN3ClusterMap.clusterIndexes())
+	// AHN3 -> AHN2
+	for (const GUInt32 ahn3Index : AHN3ClusterMap.clusterIndexes())
 	{
-		for (GUInt32 otherIndex : AHN2ClusterMap.clusterIndexes())
+		for (const GUInt32 ahn2Index : AHN2ClusterMap.clusterIndexes())
 		{
-			auto ahn2Center = AHN2ClusterMap.center2D(otherIndex);
-			auto ahn3Center = AHN3ClusterMap.center2D(index);
+			auto ahn3Center = AHN3ClusterMap.center2D(ahn3Index);
+			auto ahn2Center = AHN2ClusterMap.center2D(ahn2Index);
 			double distance = ahn2Center.Distance(&ahn3Center);
 			if (distance >= maximumDistance)
 				continue;
 
-			distances.clear();
-			for (const OGRPoint& point : AHN3ClusterMap.points(index))
+			double cmax = 0;
+			for (const OGRPoint& ahn3Point : AHN3ClusterMap.points(ahn3Index))
 			{
-				std::vector<OGRPoint> ahn3Points = AHN2ClusterMap.points(otherIndex);
-				minDistance = point.Distance(&ahn3Points[0]);
-				for (const OGRPoint& otherPoint : ahn3Points)
+				double cmin = std::numeric_limits<double>::max();
+				for (const OGRPoint& ahn2Point : AHN2ClusterMap.points(ahn2Index))
 				{
-					double dist = point.Distance(&otherPoint);
-					if (dist < minDistance)
+					double dist = ahn3Point.Distance(&ahn2Point);
+					if (dist < cmax)
 					{
-						minDistance = dist;
+						cmin = 0;
+						break;
 					}
+					cmin = std::min(dist, cmin);
 				}
-				distances.push_back(minDistance);
+				cmax = std::max(cmin, cmax);
 			}
-			ahn3HausdorffDistances.emplace(std::make_pair(std::make_pair(otherIndex, index),
-			                                              *std::max_element(distances.begin(), distances.end())));
+			ahn3HausdorffDistances.emplace(std::make_pair(std::make_pair(ahn3Index, ahn2Index), cmax));
 		}
 	}
-	
+
 	// TODO: add intermediate reporting into above loop
 	if (progress)
 		progress(0.7f, "AHN3 to AHN2 distances calculated.");
 
-  bool hasConflict;
-  do
-  {
-    hasConflict = false;
-    std::multimap<GUInt32, std::pair<GUInt32, double>> ahn3Key;
+	bool hasConflict;
+	do
+	{
+		hasConflict = false;
+		std::multimap<GUInt32, std::pair<GUInt32, double>> ahn3Key;
 
-    for (const GUInt32 index : AHN2ClusterMap.clusterIndexes())
-    {
-      double dist = std::numeric_limits<double>::max();
-      GUInt32 i = -1;
+		for (const GUInt32 ahn2Index : AHN2ClusterMap.clusterIndexes())
+		{
+			double dist = std::numeric_limits<double>::max();
+			GUInt32 i = -1;
 
-      if (std::find_if(closestClusters.begin(),
-                       closestClusters.end(),
-                       [&index](const std::pair<std::pair<GUInt32, GUInt32>, double>& p)
-                       {
-                         return p.first.first == index;
-                       })
-          == closestClusters.end())
-      {
-        for (const GUInt32 otherIndex : AHN3ClusterMap.clusterIndexes())
-        {
-          auto ahn2Res = ahn2HausdorffDistances.find({index, otherIndex});
-          auto ahn3Res = ahn3HausdorffDistances.find({index, otherIndex});
+			if (std::find_if(closestClusters.begin(),
+			                 closestClusters.end(),
+			                 [&ahn2Index](const std::pair<std::pair<GUInt32, GUInt32>, double>& p)
+			                 {
+				                 return p.first.first == ahn2Index;
+			                 })
+			    == closestClusters.end())
+			{
+				for (const GUInt32 ahn3Index : AHN3ClusterMap.clusterIndexes())
+				{
+					auto ahn2Res = ahn2HausdorffDistances.find({ahn2Index, ahn3Index});
+					auto ahn3Res = ahn3HausdorffDistances.find({ahn3Index, ahn2Index});
 
-          if (ahn2Res != ahn2HausdorffDistances.end()
-            && ahn3Res != ahn3HausdorffDistances.end())
-          {
-            double newDist = std::max(ahn2Res->second, ahn3Res->second);
+					if (ahn2Res != ahn2HausdorffDistances.end()
+					    && ahn3Res != ahn3HausdorffDistances.end())
+					{
+						double newDist = std::max(ahn2Res->second, ahn3Res->second);
 
-            if (newDist < dist &&
-                std::find_if(closestClusters.begin(),
-                             closestClusters.end(),
-                             [&otherIndex](const std::pair<std::pair<GUInt32, GUInt32>, double>& p)
-                             {
-                               return p.first.second == otherIndex;
-                             })
-                == closestClusters.end())
-            {
-              dist = newDist;
-              i = otherIndex;
-            }
-          }
-        }
+						if (newDist < dist &&
+						    std::find_if(closestClusters.begin(),
+						                 closestClusters.end(),
+						                 [&ahn3Index](const std::pair<std::pair<GUInt32, GUInt32>, double>& p)
+						                 {
+							                 return p.first.second == ahn3Index;
+						                 })
+						    == closestClusters.end())
+						{
+							dist = newDist;
+							i = ahn3Index;
+						}
+					}
+				}
 
-        if (dist <= maximumDistance)
-        {
-          ahn3Key.insert(std::make_pair(i, std::make_pair(index, dist))); // AHN3 index is key
-        }
-      }
-    }
+				if (dist <= maximumDistance)
+				{
+					ahn3Key.insert(std::make_pair(i, std::make_pair(ahn2Index, dist))); // AHN3 index is key
+				}
+			}
+		}
 
-    for (auto it = ahn3Key.begin(), end = ahn3Key.end();
-         it != end; it = ahn3Key.upper_bound(it->first))
-    {
-      GUInt32 ahn3 = it->first;
-      std::pair<GUInt32, std::pair<GUInt32, double>> minPair = *it;
-      if (ahn3Key.count(ahn3) > 1)
-      {
-        auto iter = it;
-        while (iter != end && iter->first == it->first)
-        {
-          if (minPair.second.second > iter->second.second)
-            minPair = *iter;
-          iter++;
-        }
-        hasConflict = true;
-      }
-      closestClusters.insert(std::make_pair(std::make_pair(minPair.second.first, minPair.first),
-                                            minPair.second.second));
-    }
-  }
-  while (hasConflict);
+		for (auto it = ahn3Key.begin(), end = ahn3Key.end();
+		     it != end; it = ahn3Key.upper_bound(it->first))
+		{
+			GUInt32 ahn3 = it->first;
+			std::pair<GUInt32, std::pair<GUInt32, double>> minPair = *it;
+			if (ahn3Key.count(ahn3) > 1)
+			{
+				auto iter = it;
+				while (iter != end && iter->first == it->first)
+				{
+					if (minPair.second.second > iter->second.second)
+						minPair = *iter;
+					iter++;
+				}
+				hasConflict = true;
+			}
+			closestClusters.insert(std::make_pair(std::make_pair(minPair.second.first, minPair.first),
+			                                      minPair.second.second));
+		}
+	}
+	while (hasConflict);
 
 	if (progress)
 		progress(0.8f, "Cluster map pairs calculated.");
 
-  for (GUInt32 index : AHN2ClusterMap.clusterIndexes())
-    if (std::find_if(closestClusters.begin(), closestClusters.end(),
-                     [&index](const std::pair<std::pair<GUInt32, GUInt32>, double>& item)
-                     {
-                       return item.first.first == index;
-                     }) == closestClusters.end())
-      lonelyClustersAHN2.push_back(index);
+	for (GUInt32 ahn2Index : AHN2ClusterMap.clusterIndexes())
+		if (std::find_if(closestClusters.begin(), closestClusters.end(),
+		                 [&ahn2Index](const std::pair<std::pair<GUInt32, GUInt32>, double>& item)
+		                 {
+			                 return item.first.first == ahn2Index;
+		                 }) == closestClusters.end())
+			lonelyClustersAHN2.push_back(ahn2Index);
 
-  if (progress)
-    progress(0.9f, "Lonely AHN2 clusters calculated.");
+	if (progress)
+		progress(0.9f, "Lonely AHN2 clusters calculated.");
 
-  for (GUInt32 index : AHN3ClusterMap.clusterIndexes())
-    if (std::find_if(closestClusters.begin(), closestClusters.end(),
-                     [&index](const std::pair<std::pair<GUInt32, GUInt32>, double>& item)
-                     {
-                       return item.first.second == index;
-                     }) == closestClusters.end())
-      lonelyClustersAHN3.push_back(index);
+	for (GUInt32 ahn2Index : AHN3ClusterMap.clusterIndexes())
+		if (std::find_if(closestClusters.begin(), closestClusters.end(),
+		                 [&ahn2Index](const std::pair<std::pair<GUInt32, GUInt32>, double>& item)
+		                 {
+			                 return item.first.second == ahn2Index;
+		                 }) == closestClusters.end())
+			lonelyClustersAHN3.push_back(ahn2Index);
 
 	if (progress)
 		progress(1.f, "Lonely AHN3 clusters calculated.");
